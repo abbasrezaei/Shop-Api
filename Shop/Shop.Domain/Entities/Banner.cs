afab9e419884bc94bb9b8ae8b4d7a40595a32801
rezaei <abbasrezaeiwork2022@gamil.com>
2023-12-19 16:10:33 +0330

    Completed DomainLayer

---
+++ Shop/Shop.Domain/Entities/Banner.cs
@@ -0,0 +1,42 @@
+﻿using Common.Domain.Exceptions;
+using Shop.Domain.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.Entities
+{
+    public class Banner
+    {
+        public Banner(string link, string imageName, BannerPossition pessition)
+        {
+            Guard(link, imageName);
+            Link = link;
+            ImageName = imageName;
+            Pessition = pessition;
+        }
+
+        public void Edit(string link, string imageName, BannerPossition pessition)
+        {
+            Guard(link, imageName);
+            Link = link;
+            ImageName = imageName;
+            Pessition = pessition;
+        }
+
+        public void Guard(string link , string imageName)
+        {
+            NullOrEmptyDomainDataException.CheckString(link, nameof(link));
+            NullOrEmptyDomainDataException.CheckString(imageName, nameof(imageName));
+        }
+
+
+        public string Link { get; private set; }
+
+        public string ImageName { get; private set; }
+
+        public BannerPossition Pessition { get; set; }
+    }
+}
