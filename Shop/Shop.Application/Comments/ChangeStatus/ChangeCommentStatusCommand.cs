b3a406e2ba58cadebd32983ccbc54eb0d88fb5fc
rezaei <abbasrezaeiwork2022@gamil.com>
2023-12-25 16:03:27 +0330

    Complet Comment

---
+++ Shop/Shop.Application/Comments/ChangeStatus/ChangeCommentStatusCommand.cs
@@ -0,0 +1,16 @@
+﻿using Common.Application;
+using FluentValidation;
+using Shop.Domain.CommentAgg.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Comments.ChangeStatus
+{
+    public record ChangeCommentStatusCommand(long Id,CommentStatus Status):IBaseCommand;
+
+   
+   
+}
